fae9051e64814bfddf2147265b46c47774e2b402
SDSC0623 <46809961+SDSC0623@users.noreply.github.com>
2025-07-22 01:11:29 +0800

    Update Bilibili Web Fullscreen Button HotKey.js
    Change the Hotkey from N to G

--- BilibiliScripts/Bilibili Web Fullscreen Button HotKey.js
+++ BilibiliScripts/Bilibili Web Fullscreen Button HotKey.js
@@ -1,14 +1,17 @@
 // ==UserScript==
 // @name         Bilibili Web Fullscreen Button HotKey
-// @version      1.1
-// @description  Add the ability to control the video in web fullscreen by pressing N key
+// @version      1.2
+// @description  Add the ability to control the video in web fullscreen by pressing G key
 // @author       SDSC0623
 // @match        *://www.bilibili.com/video/*
 // @icon         https://i0.hdslb.com/bfs/static/jinkela/long/images/favicon.ico
+// @namespace https://greasyfork.org/users/1496319
+// @downloadURL https://update.greasyfork.org/scripts/543002/Bilibili%20Web%20Fullscreen%20Button%20HotKey.user.js
+// @updateURL https://update.greasyfork.org/scripts/543002/Bilibili%20Web%20Fullscreen%20Button%20HotKey.meta.js
 // ==/UserScript==
 let webFullButton;
 let menuList;
-(function() {
+(function () {
     'use strict';
     menuList = document.getElementsByClassName('bpx-player-video-wrap')[0];
     menuList.addEventListener('mouseup', menuListener);
@@ -18,24 +21,24 @@ let menuList;
 
 function buttonListener() {
     webFullButton = document.getElementsByClassName('bpx-player-ctrl-web')[0];
-    if(webFullButton){
+    if (webFullButton) {
         document.querySelector('video').removeEventListener('progress', buttonListener);
         init();
     }
 }
 
 function menuListener(e) {
-    if (e.button === 2){
+    if (e.button === 2) {
         setTimeout(() => {
             document.getElementsByClassName('bpx-player-contextmenu')[0].children[3].onclick = function () {
                 let hotKeyList = document.getElementsByClassName('bpx-player-hotkey-panel-content')[0];
                 let hotKeyItem = hotKeyList.children[0].cloneNode(true);
-                hotKeyItem.children[0].innerHTML = 'N';
+                hotKeyItem.children[0].innerHTML = 'G';
                 hotKeyItem.children[1].innerHTML = '网页全屏/退出网页全屏';
                 let index = 0;
-                for (index = 0; index < hotKeyList.children.length; index++){
+                for (index = 0; index < hotKeyList.children.length; index++) {
                     let temp = hotKeyList.children[index];
-                    if (temp.children[0].innerHTML === 'F' && temp.children[1].innerHTML === '全屏/退出全屏'){
+                    if (temp.children[0].innerHTML === 'F' && temp.children[1].innerHTML === '全屏/退出全屏') {
                         break;
                     }
                 }
@@ -47,11 +50,11 @@ function menuListener(e) {
 }
 
 function init() {
-    webFullButton.onclick = function(){
+    webFullButton.onclick = function () {
         addToolTip();
     }
     addToolTip();
-    console.log('Bilibili Web Fullscreen Button HotKey 加载完成，绑定快捷键为 N ')
+    console.log('Bilibili Web Fullscreen Button HotKey 加载完成，绑定快捷键为 G 键')
 }
 
 async function keyboardPress(e) {
@@ -63,7 +66,7 @@ async function keyboardPress(e) {
         activeElement.isContentEditable
     );
 
-    if (e.code === 'KeyN' && !e.ctrlKey && !e.altKey && !e.metaKey && !isEditable) {
+    if (e.code === 'KeyG' && !e.ctrlKey && !e.altKey && !e.metaKey && !isEditable) {
         await webFullButton.click();
         addToolTip();
     }
@@ -71,8 +74,8 @@ async function keyboardPress(e) {
 
 function addToolTip() {
     document.getElementsByClassName('bpx-player-tooltip-title').forEach(temp => {
-        if(temp.innerHTML === '网页全屏' || temp.innerHTML === '退出网页全屏'){
-            temp.innerHTML += ' (n)';
+        if (temp.innerHTML === '网页全屏' || temp.innerHTML === '退出网页全屏') {
+            temp.innerHTML += ' (g)';
         }
     })
 }
